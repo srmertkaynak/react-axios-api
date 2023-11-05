1171f68c898461050bcc4a69db1b9843c28b84e4
mertkaynak <kaynak.mert35@gmail.com>
2023-11-05 18:37:46 +0300

    Latest Image Update

--- src/Components/ImageItem.jsx
+++ src/Components/ImageItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageItem = ({ image }) => {
   console.log(image);
-  return <div>Image Item</div>;
+  return <div>Image Items</div>;
 };
 
 export default ImageItem;
